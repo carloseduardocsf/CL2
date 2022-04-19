b9cd56f30473c6bf5fcd795c7647206981c8a5f6
Carlos Eduardo Cavalcanti <69211390+carloseduardocsf@users.noreply.github.com>
2022-04-18 21:46:20 -0300

    .

--- ex07/ex07.sv
+++ ex07/ex07.sv
@@ -1,4 +1,4 @@
-module projeto05(input [7:0] x, input [7:0] y, output menor, maior, igual);
+module ex07(input [7:0] x, input [7:0] y, output menor, maior, igual);
 	
 assign menor = (x<y);
 assign maior = (x>y);
